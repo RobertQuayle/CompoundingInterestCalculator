f4f97c89c1432e4970c8674e961f3b6190a3872a
Cameron Quayle <robertquaylebusiness@gmail.com>
2022-03-12 02:48:00 +0000

    Initial Commit.

---
+++ CIUtil/Compound.cs
@@ -0,0 +1,41 @@
+﻿namespace CIUtil
+{
+
+    /*
+
+    Compound Class: Collection of methods for compounding interest functionality.
+
+    COMPLETED - Monthly() Method: Takes 4 parameters. Contributions added first, compounded monthly.
+    TODO - Yearly() Method: Takes 4 parameters. Contributions added first, compounded yearly.
+    TODO - Reverse() Method: Takes ? parameters. Starts at goal, back-tracks informing user on actions to hit target.
+    TODO - APR() Method: Returns average APR% for well known asset classes, S&P500 for example.
+
+     */
+
+    public static class Compound
+    {
+
+        public static string Monthly(decimal startAmount, decimal monthlyCont, decimal annualPercent, int years)
+        {
+            decimal total = startAmount;
+
+            int totalMonths = years * 12;
+
+            decimal monthlyPercent = annualPercent / 12;
+
+            for (int i = 0; i < totalMonths; i++)
+            {
+                total += monthlyCont;
+                total = (total / 100) * (monthlyPercent + 100); 
+            }
+
+            decimal result = decimal.Round(total, 2);
+
+            // "C" formats output to currency.
+            return result.ToString("C");
+
+        }
+
+    }
+
+}
---
+++ CIUtil/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using CIUtil;
+
+namespace CICalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //NOTE: Changes currency formatting of ToString() from $ to £
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
+
+            Console.WriteLine("Compound Interest Calculator v1.0.0");
+
+            decimal initialValue = Menu.GetInitialValue();
+
+            decimal monthlyCont = Menu.GetMonthlyCont();
+
+            decimal apr = Menu.GetAPR();
+
+            int years = Menu.GetYears();
+
+            string result = Compound.Monthly(initialValue,monthlyCont,apr,years);
+
+            Console.WriteLine($"\nAfter {years} year(s)");
+            Console.WriteLine($"Your initial investment of £{initialValue}");
+            Console.WriteLine($"With monthly contributions of £{monthlyCont}");
+            Console.WriteLine($"At {apr}% APR");
+            Console.WriteLine($"Is now worth {result}");
+        }
+
+    }
+
+    /*
+     Menu Class: Generic-reuseable get methods for user inputs,
+     then parsing them to the correct type.
+     TODO: Add Try/Catch validation.
+     */
+
+    public static class Menu
+    {
+
+        public static decimal GetInitialValue()
+        {
+            Console.WriteLine("\nPlease input your starting amount below:");
+            Console.Write("£");
+            decimal initialValue = decimal.Parse(Console.ReadLine());
+            return initialValue;
+        }
+
+        public static decimal GetMonthlyCont()
+        {
+            Console.WriteLine("\nPlease input your monthly contributions:");
+            Console.Write("£");
+            decimal monthlyCont = decimal.Parse(Console.ReadLine());
+            return monthlyCont;
+        }
+
+        public static decimal GetAPR()
+        {
+            Console.WriteLine("\nPlease input the annual percentage rate (APR):");
+            Console.Write("%");
+            decimal apr = decimal.Parse(Console.ReadLine());
+            return apr;
+        }
+
+        public static int GetYears()
+        {
+            Console.WriteLine("\nPlease input the number of years to calculate:");
+            Console.Write("Years = ");
+            int years = int.Parse(Console.ReadLine());
+            return years;
+        }
+
+    }
+
+}
