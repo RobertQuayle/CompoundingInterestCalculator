88bb522f9f6ac684d9e470ba79b8bc9c1319c371
Cameron Quayle <robertquaylebusiness@gmail.com>
2022-03-13 16:09:48 +0000

    Added README.txt

--- CIUtil/CompoundingFunctionality/Compound.cs
+++ CIUtil/CompoundingFunctionality/Compound.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace CIUtil
+﻿namespace CIUtil
 {
 
     /*
@@ -34,25 +32,6 @@ namespace CIUtil
             decimal result = decimal.Round(total, 2);
 
             // "C" formats output to currency.
-            return result.ToString("C");
-
-        }
-
-    }
-
-    public static class Decompound
-    {
-
-        public static string Monthly(double goalAmount, int currentAge, int retirementAge, double annualPercentage, double monthlyCont = 0)
-        {
-            int compoundTimes = (retirementAge - currentAge) * 12;
-
-            double percentage = annualPercentage / 100;
-
-            double regMonthlyCont = (Math.Pow(1 + percentage / 12, compoundTimes) - 1) / (percentage / 12);
-
-            double result = Math.Round((goalAmount / regMonthlyCont),2);
-
             return result.ToString("C");
 
         }
